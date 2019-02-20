e95febc2aee622585ce7b00f83232499d87ab8e4
Hugues McNeil-Duval <hugues.mcneil-duval@alithya.com>
2019-02-19 20:55:51 -0500

    Tsting pipeline

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 HOST="root@superheroes.philippeduval.ca"
 DIRECTORY="/var/app/superheroes.philippeduval.ca/"
+ARCHIVE="super-heroes-build-insertversionnumberhere.tar.gz"
 
 scp super-heroes-build-insertversionnumberhere.tar.gz $HOST:$DIRECTORY
 
 ssh $HOST "
+cd $DIRECTORY
+tar -xzf $ARCHIVE -C bin
 pm2 reload superheroes.philippeduval.ca
 "
